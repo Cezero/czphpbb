e847db34fdd1ace52bc564388089f8542913a4c6
BuildTools <unconfigured@null.spigotmc.org>
2022-03-21 09:55:50 -0400

    starting over

--- eqdkp/styles/prosilver/template/roster_raidlist.js
+++
@@ -1,3 +0,0 @@
-$('.confirmation').on('click', function () {
-		return confirm('This will delete this raid and all associated DKP and items awarded that are associated with this raid.  Are you sure?');
-		});
