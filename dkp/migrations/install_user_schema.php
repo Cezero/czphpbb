416451cf1afa525b75a68127883c0c31b50931be
BuildTools <unconfigured@null.spigotmc.org>
2022-03-21 20:23:39 -0400

    add all custom tables into install_user_schema

--- dkp/migrations/install_user_schema.php
+++ dkp/migrations/install_user_schema.php
@@ -25,49 +25,169 @@ class install_user_schema extends \phpbb\db\migration\migration
 
 	public function update_schema()
 	{
-		return array(
-			'add_tables'		=> array(
-				// user character table
-				$this->table_prefix . 'czphpbb_dkp_characters'	=> array(
-					'COLUMNS'		=> array(
-						'char_id'			=> array('UINT', null, 'auto_increment'),
-						'user_id'			=> array('UINT', null),
-						'char_name'			=> array('VCHAR:255', ''),
-						'char_class'			=> array('TINT:8', 0), // 0 = WAR, 1 = CLR, etc
-					),
-					'PRIMARY_KEY'	=> 'char_id',
-					'INDEX' => array('user_id', 'char_id'),
-				),
-			),
-			'add_columns' => array(
-				$this->table_prefix . 'users' => array(
-					'czphpbb_dkp_start_date' => array('TIMESTAMP'),
-					'czphpbb_dkp_current_dkp' => array('UINT', 0),
-					'czphpbb_dkp_tick_cnt' => array('UINT', 0),
-					'czphpbb_dkp_ninety_cnt' => array('UINT', 0),
-					'czphpbb_dkp_sixty_cnt' => array('UINT', 0),
-					'czphpbb_dkp_thirty_cnt' => array('UINT', 0),
-				),
-			),
-		);
+		return [
+			'add_tables'		=> [
+				$this->table_prefix . 'lucy_itemlist' => [
+					'COLUMNS' => [
+						'id' => ['UINT', 0],
+						'name' => ['CHAR:80', ''],
+						'lucylink' => ['VCHAR', ''],
+					],
+					'KEYS' => [
+						'itemlist_name' => [null, ['name']],
+						'itemlist_id' => [null, ['id']]
+					]
+				],
+
+				$this->table_prefix . 'czphpbb_dkp_adjustment' => [
+					'COLUMNS' => [
+						'user_id' => ['UINT', 0],
+						'value' => ['UINT', 0],
+						'description' => ['VCHAR', ''],
+						'entered_by' => ['UINT', 0],
+						'entered_on' => ['UINT', 0],
+						'second_pool' => ['TINT:8', 0],
+						'adj_id' => ['UINT', null, 'auto_increment'],
+					],
+					'PRIMARY_KEY' => 'adj_id',
+					'KEYS' => [
+						'adj_userid_pool' => [null, ['user_id', 'second_pool']]
+					]
+				],
+
+				$this->table_prefix . 'czphpbb_dkp_char_roles' => [
+					'COLUMNS' => [
+						'role_id' => ['TINT:8', ''],
+						'role_name' => ['CHAR:20', ''],
+					],
+				],
+
+				$this->table_prefix . 'czphpbb_dkp_characters' => [
+					'COLUMNS' => [
+						'char_id' => ['UINT', null, 'auto_increment'],
+						'user_id' => ['UINT', 0],
+						'char_name' => ['VCHAR', ''],
+						'char_class' => ['TINT:8', 0],
+						'deleted' => ['BOOL'],
+						'role' => ['TINT:8', 0],
+					],
+					'PRIMARY_KEY' => 'char_id',
+					'KEYS' => [
+						'char_uid_cid' => [null, ['user_id','char_id']],
+						'char_uid_role' => [null, ['user_id','role']]
+					]
+				] 
+
+				$this->table_prefix . 'czphpbb_dkp_log' => [
+					'COLUMNS' => [
+						'log_id' => ['UINT', null, 'auto_increment'],
+						'entered_by' => ['UINT', null],
+						'entered_on' => ['UINT', null],
+						'description' => ['VCHAR', null],
+					],
+					'PRIMARY_KEY' => 'log_id'
+				],
+
+				$this->table_prefix . 'czphpbb_dkp_raid' => [
+					'COLUMNS' => [
+						'raid_id' => ['UINT', null, 'auto_increment'],
+						'day' => ['UINT', 0],
+						'start' => ['DECIMAL', 0],
+						'end' => ['DECIMAL', 0],
+						'description' => ['VCHAR', ''],
+						'entered_by' => ['UINT', 0],
+						'entered_on' => ['UINT', 0],
+						'raid_ticks' => ['UINT', 0],
+						'seconds_earn' => ['TINT:8', 0],
+						'double_dkp' => ['TINT:8', 0],
+					],
+					'PRIMARY_KEY' => ['raid_id']
+				],
+
+				$this->table_prefix . 'czphpbb_dkp_raid_attendance' => [
+					'COLUMNS' => [
+						'user_id' => ['UINT', 0],
+						'raid_id' => ['UINT', 0],
+						'entered_on' => ['UINT', 0],
+						'entered_by' => ['UINT', 0],
+						'char_id' => ['UINT', 0],
+						'char_role' => ['TINT:8', 0],
+						'char_times' => ['VCHAR:100', ''],
+						'earned_dkp' => ['UINT', 0],
+						'ticks' => ['UINT', 0],
+					],
+					'KEYS' => [
+						'unique_index' => ['UNIQUE', ['user_id','raid_id','char_id']],
+						'user_id' => [null, ['user_id','raid_id']],
+						'rl_userid_pool' => [null, ['user_id','char_role']]
+					]
+				],
+
+				$this->table_prefix . 'czphpbb_dkp_raid_loot' => [
+					'COLUMNS' => [
+						'user_id' => ['UINT', 0],
+						'raid_id' => ['UINT', 0],
+						'char_id' => ['UINT', 0],
+						'cost' => ['UINT', 0],
+						'entered_by' => ['UINT', 0],
+						'entered_on' => ['UINT', 0],
+						'lucy_id' => ['UINT', 0],
+						'second_pool' => ['BOOL', 0],
+					],
+					'PRIMARY_KEY' => 'user_id',
+					'KEYS' => [null, ['user_id','second_pool']]
+				],
+
+				$this->table_prefix . 'czphpbb_dkp_rollover' => [
+					'COLUMNS' => [
+						'user_id' => ['UINT', ''],
+						'dkp' => ['UINT', null],
+						'full' => ['UINT', null],
+						'ninety' => ['UINT', null],
+						'sixty' => ['UINT', null],
+						'thirty' => ['UINT', null],
+					],
+					'PRIMARY_KEY' => 'user_id'
+				],
+
+			],
+			'add_columns' => [
+				$this->table_prefix . 'users' => [
+					'czphpbb_dkp_start_date'	=> ['TIMESTAMP'],
+					'czphpbb_dkp_current_dkp'	=> ['UINT', 0],
+					'czphpbb_dkp_tick_cnt'		=> ['UINT', 0],
+					'czphpbb_dkp_ninety_cnt'	=> ['UINT', 0],
+					'czphpbb_dkp_sixty_cnt'		=> ['UINT', 0],
+					'czphpbb_dkp_thirty_cnt'	=> ['UINT', 0],
+				],
+			],
+			];
 	}
 
 	public function revert_schema()
 	{
-		return array(
-			'drop_columns' => array(
-				$this->table_prefix . 'users' => array(
+		return [
+			'drop_columns' => [
+				$this->table_prefix . 'users' => [
 					'czphpbb_dkp_start_date',
 					'czphpbb_dkp_current_dkp',
 					'czphpbb_dkp_tick_cnt',
 					'czphpbb_dkp_ninety_cnt',
 					'czphpbb_dkp_sixty_cnt',
 					'czphpbb_dkp_thirty_cnt',
-				),
-			),
-			'drop_tables'		=> array(
+				],
+			],
+			'drop_tables'		=> [
+				$this->table_prefix . 'lucy_itemlist',
+				$this->table_prefix . 'czphpbb_dkp_adjustment',
+				$this->table_prefix . 'czphpbb_dkp_char_roles',
 				$this->table_prefix . 'czphpbb_dkp_characters',
-			),
-		);
+				$this->table_prefix . 'czphpbb_dkp_log',
+				$this->table_prefix . 'czphpbb_dkp_raid',
+				$this->table_prefix . 'czphpbb_dkp_raid_attendance',
+				$this->table_prefix . 'czphpbb_dkp_raid_loot',
+				$this->table_prefix . 'czphpbb_dkp_rollover',
+			],
+		];
 	}
 }
